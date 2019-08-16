0e9d19d3e94d8630cc5d721ff64382a463a455c8
Calvin McDowell <calvinmac40@hotmail.com>
2019-08-16 22:42:45 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,11 @@
-# def countdown(number)
-#   number
-#   while number > 0
-#   "#{number} SECONDS(S)"
-#   "HAPPY NEW YEAR!"
-#   number += 1
-# end
-# end
+def countdown(number)
+  number
+  while number > 0
+  "#{number} SECONDS(S)"
+  "HAPPY NEW YEAR!"
+  number += 1
+end
+end
 
 
 
